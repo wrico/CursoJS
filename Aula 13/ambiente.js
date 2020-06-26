d866e6e81701f5e83d764b9c0fa52bdb6fbda193
Wilson Maldonado <wmaldonado@outlook.com.br>
2020-06-26 11:09:23 -0300

    Testes do GIT

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,24 @@
+
+c = 0
+while (c<6) {
+    console.log(`voltei ${c}`)
+    ++c
+}
+
+console.log(`valor do c até aqui ${c}`)
+
+while (c>0) {
+    console.log(`voltei ${c}`)
+    --c
+}
+
+c=10
+console.log(`valor do C Fora ${c}`)
+
+do {  
+
+    console.log(`valor do C ${c}`)
+    --c
+
+
+} while (c>0)
