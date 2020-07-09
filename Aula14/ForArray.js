3356b16d7476cc25417872602c68e8cd4a288ba8
Wilson Maldonado <wmaldonado@outlook.com.br>
2020-07-09 16:44:50 -0300

    Exercícios
    - Figurinhas no contador do EX16
    - Uso do Select no Ex17
    - Array no ForArray

---
+++ Aula14/ForArray.js
@@ -0,0 +1,12 @@
+// for c<= 10 do....
+
+var cars = ["BMW", "Volvo", "Saab", "Ford", "Fusca", "Gol"]
+
+var text = ""
+
+
+for (var c=0; c<6; ++c)
+{
+ text += cars[c] 
+ console.log(c,"-" ,cars[c])
+}
--- Aula14/Fori.js
+++ Aula14/Fori.js
@@ -1,12 +1,7 @@
-// for c<= 10 do....
+for (i=10; i>1 ; --i ) 
+{ console.log( ` Valor de I -> ${i} `)
 
-var cars = ["BMW", "Volvo", "Saab", "Ford", "Fusca", "Gol"]
+console.log(`I ao quadrado ${i**2} `)
+console.log(` 10 % de i ${1 - (i * 0.1)} `)
 
-var text = ""
-
-
-for (var c=0; c<6; ++c)
-{
- text += cars[c] 
- console.log(c,"-" ,cars[c])
-}
+console.log('Fim') } 
