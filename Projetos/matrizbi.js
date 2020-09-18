3f11378f15dd5463259576fceeea85d29f0002cb
Wilson Maldonado <wmaldonado@outlook.com.br>
2020-09-18 15:35:14 -0300

    Create matrizbi.js

---
+++ Projetos/matrizbi.js
@@ -0,0 +1,12 @@
+// Matriz Mutidimensional 
+
+let ativ = [ ["Wilson", "Programador","primeiro"], ["Rico","Gerente","segundo"], ["Maldonado","Scrum", "terceiro"] ]
+console.table(ativ)
+console.log(ativ[0][0], ativ[0][1], " e ", ativ[2][1], " Master")
+
+var i=0
+while (ativ[i]) {
+    console.log(i,"-" ,ativ[i],"-",ativ[i+1],"-",ativ[i+2])
+    i++;
+  }
+
