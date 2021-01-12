2b98241030eb9def53d14cca34f197aec8f2a49b
Wilson Maldonado <wmaldonado@outlook.com.br>
2021-01-12 13:48:14 -0300

    Teste.js
    Teste JavaScript Local

---
+++ Projetos/teste.js
@@ -0,0 +1,21 @@
+let x = new Number(5,9)
+
+console.log(x.toExponential(5))
+
+console.log(x.toFixed(2))
+
+console.log(x.valueOf())
+
+console.log(x.toPrecision(10))
+
+console.log(Math.pow(x,3))
+
+console.log(Math.floor(x))
+
+var meunome = "w\ni\nl"
+console.log(meunome[0])
+console.log(meunome)
+console.log('tamanho da minha string ',meunome.length)
+
+
+
