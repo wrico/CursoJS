4aa61af5cea3303a5951f0fd0060d17088fe7eb8
Wilson Maldonado <wmaldonado@outlook.com.br>
2021-11-10 16:22:36 -0300

    Atualização
    Atualização

---
+++ Projetos/Corrida/CalculaIMC.js
@@ -0,0 +1,13 @@
+// Calcula IMC com função de callback
+
+var peso=0, altura=0
+
+var imc = function(peso, altura){
+    return peso/altura**2
+    }
+
+console.log("Meu IMC hoje 09/11/2021", imc(83,1.83).toFixed(2), " e Altura 1.83")
+
+for (var i = 90; i >= 69; i=i-0.20) {
+    console.log("IMCs para ", i.toFixed(2) ," quilos ->", imc(i,1.83).toFixed(2) );
+}
--- Projetos/Corrida/calculasemanadoano.js
+++ Projetos/Corrida/calculasemanadoano.js
@@ -7,3 +7,15 @@ Date.prototype.getWeekNumber = function(){
   };
   
   console.log('The current ISO week number is ' + new Date().getWeekNumber());
+
+  /*
+new Date().getDate()          // Get the day as a number (1-31)
+new Date().getDay()           // Get the weekday as a number (0-6)
+new Date().getFullYear()      // Get the four digit year (yyyy)
+new Date().getHours()         // Get the hour (0-23)
+new Date().getMilliseconds()  // Get the milliseconds (0-999)
+new Date().getMinutes()       // Get the minutes (0-59)
+new Date().getMonth()         // Get the month (0-11)
+new Date().getSeconds()       // Get the seconds (0-59)
+new Date().getTime()          // Get the time (milliseconds since January 1, 1970)
+  */
