03069ffdfe61b2bbc903286780f64c8e84a14fbe
Wilson Maldonado <wmaldonado@outlook.com.br>
2021-11-11 13:12:49 -0300

    Copia da pasta
    Copia da pasta

---
+++ Programadorabordo/01-variaveis.js
@@ -0,0 +1,21 @@
+
+var nome = 'Wil';
+console.log(nome);
+
+var idade = 59;
+console.log(idade);
+
+idade = 27;
+console.log(idade);
+
+var total = idade + 10;
+console.log(total);
+
+var total = 50;
+console.log(50);
+
+let pais = 'Brasil';
+console.log(pais);
+
+const ultimoNome = 'Rico';
+console.log(ultimoNome);
